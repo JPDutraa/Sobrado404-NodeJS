e8cc98eee8c499ab91697835de9a560385a4b094
João Pedro Dutra <j.pedro.1@hotmail.com>
2023-04-06 22:31:22 -0300

    Att 2

---
+++ backend/src/services/user/CreateUserService.ts
@@ -0,0 +1,19 @@
+interface UserRequest{
+    name: string;
+    email: string;
+    password: string;
+}
+
+class CreateUserService{
+    async execute({name, email, password}: UserRequest){
+        // Check if user exists
+        // Hash password
+        // Save user in DB
+
+        console.log(name);
+
+        return {name: name}
+    }
+}
+
+export { CreateUserService };
